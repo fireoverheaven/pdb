6e26942bad7382c4a97da8ac52c339d782eb97ed
DJ Amireh <dj@fireoh.com>
2023-12-16 19:06:15 -0500

    Sat Dec 16 19:06:15 2023

--- finddupes.go
+++ finddupes.go
@@ -7,10 +7,13 @@ import (
 )
 
 func InitDB(filename string) *storm.DB {
+  log.Printf("Opening db at '%s'", filename)
   db, err := storm.Open(filename)
   if err != nil {
     log.Fatalf("Couldn't open storm db at '%s'", filename)
   }
+
+  log.Printf("Initializing db")
   err = db.Init(&FileMetadata{})
   if err != nil {
     log.Fatalf("Couldn't initialize db ('%s') as FileMetadata", filename)
@@ -19,15 +22,20 @@ func InitDB(filename string) *storm.DB {
   return db
 }
 
-func ReindexDB(db *storm.DB) {
+func ReindexDB(db *storm.DB) error {
+  log.Printf("Reindexing db")
   err := db.ReIndex(&FileMetadata{})
   if err != nil {
     log.Fatalf("Couldn't reindex: %s", err)
   }
+
+  log.Printf("Committing db")
   err = db.Commit()
   if err != nil {
     log.Fatalf("Couldn't commit: %s", err)
   }
+
+  return nil
 }
 
 func PrintBySize(db *storm.DB) {
