763176bbaa39886084263719d6eae14858b98644
DJ Amireh <dj@fireoh.com>
2023-12-18 06:18:48 -0500

    Mon Dec 18 06:18:48 2023

--- finddupes.go
+++ finddupes.go
@@ -62,6 +62,7 @@ func PrintBySize(db *storm.DB) {
     for _, r2 := range daisyrecords {
       if r2.Size == record.Size && r2.Filename == record.Filename {
         f.WriteString(r2.Path)
+        f.WriteString("\n")
       }
     }
   }
